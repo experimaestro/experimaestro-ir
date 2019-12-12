75ebbee3f2d758622496a47b8007626287d59e79
Benjamin Piwowarski <benjamin@piwowarski.fr>
2019-12-12 18:15:12 +0100

    Progress report

--- experimaestro_ir/anserini.py
+++ experimaestro_ir/anserini.py
@@ -1,8 +1,12 @@
+import asyncio
 from pathlib import Path
 import os
+import sys
+from shlex import quote as shquote
+import re
 
 from datamaestro_text.data.trec import TipsterCollection, AdhocDocuments
-from experimaestro import Task, Argument, Typename, PathArgument, parse_commandline
+from experimaestro import Task, Argument, Typename, PathArgument, parse_commandline, progress
 from experimaestro_ir import NAMESPACE
 
 ANSERINI_NS = Typename("ir.anserini")
@@ -18,13 +22,17 @@ class IndexCollection:
     CLASSPATH = "io.anserini.index.IndexCollection"
     
     def execute(self):
-        import pyserini
+        from pyserini.setup import configure_classpath
+        configure_classpath(os.environ["ANSERINI_CLASSPATH"])
+        from jnius_config import get_classpath
 
-        command = []
-        command.extend(["-index", self.index_path.localpath(), "-threads", self.threads])
+        command = ["{}/bin/java".format(os.environ["JAVA_HOME"]), "-cp"]
+        command.append(":".join(get_classpath()))
+        command.append(IndexCollection.CLASSPATH)
+        command.extend(["-index", self.index_path, "-threads", self.threads])
       
         if isinstance(self.collection, TipsterCollection):
-            command.extend(["-collection", "TrecCollection",  "-generator", "JsoupGenerator", "-input", self.collection.path.localpath()])
+            command.extend(["-collection", "TrecCollection",  "-generator", "JsoupGenerator", "-input", self.collection.path])
 
         if self.storePositions:
             command.append("-storePositions")
@@ -34,13 +42,38 @@ class IndexCollection:
             command.append("-storeRawDocs")
 
 
-        from pyserini.setup import configure_classpath
-        configure_classpath(os.environ["ANSERINI_CLASSPATH"])
-        from jnius import autoclass
 
-        print(command)        
-        indexCollection = autoclass(IndexCollection.CLASSPATH)
-        indexCollection.main([str(s) for s in command])
+        # Index and keep track of progress through regular expressions
+        RE_FILES = re.compile(rb""".*index\.IndexCollection \(IndexCollection.java:\d+\) - (\d+) files found""")
+        RE_FILE = re.compile(rb""".*index\.IndexCollection\$LocalIndexerThread \(IndexCollection.java:\d+\).* docs added.""")
+        print(RE_FILES)
+        async def run(command):
+            proc = await asyncio.create_subprocess_exec(
+                *command,
+                stderr=None,
+                stdout=asyncio.subprocess.PIPE)
 
-if __name__ == "__main__":
-    parse_commandline()
+            nfiles = -1
+            indexedfiles = 0
+
+            while True:
+                data = await proc.stdout.readline()
+                
+                if not data:
+                    break
+
+                m = RE_FILES.match(data)
+                if m:
+                    nfiles = int(m.group(1).decode("utf-8"))
+                    print("%d files to index" % nfiles)
+                elif RE_FILE.match(data):
+                    indexedfiles += 1
+                    progress(indexedfiles / nfiles)
+                else:
+                    print(data.decode("utf-8"))
+
+            await proc.wait()
+            sys.exit(proc.returncode)
+                
+            
+        asyncio.run(run([str(s) for s in command]))
