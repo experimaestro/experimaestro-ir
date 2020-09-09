ea9f443c6ac8644b7ab70c1f8cc14addb7f0eeec
Benjamin Piwowarski <benjamin@piwowarski.fr>
2020-09-09 11:59:59 +0200

    Moved user agreement

--- experimaestro_ir/dm/config/ca/uwaterloo/jimmylin/anserini.py
+++ experimaestro_ir/dm/config/ca/uwaterloo/jimmylin/anserini.py
@@ -10,15 +10,15 @@ from experimaestro_ir.dm.data.anserini import Index
 from hashlib import md5
 
 
-@tardownloader(
-    "index",
-    url="https://git.uwaterloo.ca/jimmylin/anserini-indexes/raw/master/index-robust04-20191213.tar.gz",
-    checker=HashCheck("15f3d001489c97849a010b0a4734d018", md5),
-)
 @useragreement(
     """Robust04 dataset.
 Please confirm you agree to the authors' data usage stipulations found at
 https://trec.nist.gov/data/cd45/index.html"""
+)
+@tardownloader(
+    "index",
+    url="https://git.uwaterloo.ca/jimmylin/anserini-indexes/raw/master/index-robust04-20191213.tar.gz",
+    checker=HashCheck("15f3d001489c97849a010b0a4734d018", md5),
 )
 @dataset(Index)
 def robust04(index):
