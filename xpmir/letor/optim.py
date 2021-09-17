b6579406eb3a2191d6ec5f54f3da978d97f6a58b
Benjamin Piwowarski <benjamin@piwowarski.fr>
2021-09-17 17:57:47 +0200

    Adding AdamW

--- xpmir/letor/optim.py
+++ xpmir/letor/optim.py
@@ -12,3 +12,12 @@ class Adam(Optimizer):
         from torch.optim import Adam
 
         return Adam(parameters, lr=self.lr)
+
+
+class AdamW(Optimizer):
+    lr: Param[float] = 1e-3
+
+    def __call__(self, parameters):
+        from torch.optim import AdamW
+
+        return AdamW(parameters, lr=self.lr)
