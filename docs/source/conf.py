0da755f4e1ddc938f808b861757b4f2fb0b2edc1
Benjamin Piwowarski <benjamin@piwowarski.fr>
2022-02-04 18:50:18 +0100

    ammend

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,9 @@ import re
 from experimaestro.mkdocs.metaloader import DependencyInjectorFinder
 
 DependencyInjectorFinder.install(
-    re.compile(r"^(torch|faiss|pandas|bs4|pytorch_transformers|pytrec_eval|apex)($|\.)")
+    re.compile(
+        r"^(torch|faiss|pandas|bs4|pytorch_transformers|pytrec_eval|apex|pytorch\_lightning|transfomers)($|\.)"
+    )
 )
 
 
