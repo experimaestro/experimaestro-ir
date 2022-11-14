210430320beff2acbbea14f43bcc46f55d05e367
Benjamin Piwowarski <benjamin@piwowarski.fr>
2022-11-14 08:13:09 +0100

    Switched to setup.cfg and pyproject.toml

--- setup.py
+++ setup.py
@@ -16,43 +16,16 @@ def get_description():
     return desc
 
 
+# Handles dependencies here
 basepath = Path(__file__).parent
 install_requires = (basepath / "requirements.txt").read_text()
 install_requires = re.sub(
     r"^(git\+https.*)egg=([_\w-]+)$", r"\2@\1", install_requires, 0, re.MULTILINE
 )
 
-# Removes dependencies if building documentation
 if os.environ.get("DOC_BUILDING", 0) == "1":
     install_requires = re.sub(r"SKIP_DOCBUILD.*", r"", install_requires, 0, re.DOTALL)
 
 setup(
-    name="experimaestro-ir",
-    python_requires=">=3.8",
-    description="Experimaestro common module for IR experiments",
-    long_description=get_description(),
-    long_description_content_type="text/markdown",
-    author="Benjamin Piwowarski",
-    author_email="benjamin@piwowarski.fr",
-    url="https://github.com/bpiwowar/experimaestro-ir",
-    packages=find_packages(exclude=["test*", "tools"]),
-    use_scm_version=True,
-    license="GPL 3.0",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
     install_requires=install_requires,
-    setup_requires=["setuptools_scm", "setuptools >=30.3.0"],
-    entry_points={
-        "datamaestro.repositories": {
-            "ir = xpmir:Repository",
-            "irds=xpmir.datasets.irds:Repository",
-        }
-    },
 )
