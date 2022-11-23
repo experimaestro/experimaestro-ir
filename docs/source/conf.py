a6c6ce523c16c3a299aba16238b6e36030a2229c
Benjamin Piwowarski <benjamin@piwowarski.fr>
2022-11-23 18:25:05 +0100

    Fix
    [ci skip]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,7 @@ extensions = [
     # Named tuples
     "sphinx_toolbox.more_autodoc.autonamedtuple",
     # Code
-    "view_code",
+    "sphinx.ext.view_code",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
