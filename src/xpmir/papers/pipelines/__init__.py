f6a3e168cc59a73149e37cc727953a5a68b938a5
Benjamin Piwowarski <benjamin@piwowarski.fr>
2023-03-23 10:35:01 +0100

    Documentation

--- src/xpmir/papers/pipelines/__init__.py
+++ src/xpmir/papers/pipelines/__init__.py
@@ -5,10 +5,20 @@ from omegaconf import MISSING
 @define(kw_only=True)
 class PaperExperiment:
     id: str = MISSING
-    """The experiment ID"""
+    """The experiment ID
+
+    The ID should be unique, and might be used when exporting the model e.g. to
+    HuggingFace
+    """
 
     title: str = ""
-    """The model title"""
+    """The model title
+
+    The title is used to generate report
+    """
 
     description: str = ""
-    """A description of the model"""
+    """A description of the model
+
+    This description is used to generate reports
+    """
