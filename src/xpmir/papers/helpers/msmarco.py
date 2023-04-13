929290cb9f7d332ce69f7e854c4b3c351968c655
Yuxuan ZONG <yuxuan.zong@sorbonne-universite.fr>
2023-04-13 17:42:50 +0200

    splade refactor

--- src/xpmir/papers/helpers/msmarco.py
+++ src/xpmir/papers/helpers/msmarco.py
@@ -32,6 +32,12 @@ class RerankerMSMarcoV1Configuration(NeuralIRExperiment):
     validation: ValidationSample = Factory(ValidationSample)
 
 
+class DualMSMarcoV1Configuration(NeuralIRExperiment):
+    """Configuration for sparse retriever"""
+
+    validation: ValidationSample = Factory(ValidationSample)
+
+
 # MsMarco v1
 
 v1_passages: Callable[[], AdhocDocuments] = partial_cache(
