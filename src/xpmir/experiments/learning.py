1091555bf0348e31ea01885b1d742b5f211da1a7
Benjamin Piwowarski <benjamin@piwowarski.fr>
2024-05-12 16:41:09 +0200

    Don't remove content when not running normally

--- src/xpmir/experiments/learning.py
+++ src/xpmir/experiments/learning.py
@@ -1,4 +1,3 @@
-import inspect
 from functools import cached_property
 
 from xpmir.experiments import ExperimentHelper
@@ -10,17 +9,6 @@ class LearningExperimentHelper(ExperimentHelper):
     def tensorboard_service(self) -> TensorboardService:
         """Returns a tensorboard service"""
         return self.xp.add_service(TensorboardService(self.xp.resultspath / "runs"))
-
-    # TODO: remove when in experimaestro
-    @classmethod
-    def decorator(cls, *args, **kwargs):
-        if len(args) == 1 and len(kwargs) == 0 and inspect.isfunction(args[0]):
-            return cls(callable)
-
-        def wrapper(callable):
-            return cls(callable)
-
-        return wrapper
 
 
 learning_experiment = LearningExperimentHelper.decorator
