498e95dd9ffd3793902e8bb743b4ab12a73269bd
Benjamin Piwowarski <benjamin@piwowarski.fr>
2024-06-03 08:36:14 +0200

    feat: new parameter sharer module

--- src/xpmir/learning/hooks.py
+++ src/xpmir/learning/hooks.py
@@ -21,7 +21,30 @@ class LayerFreezer(InitializationTrainingHook):
     def after(self, state: TrainState):
         if not self._initialized:
             self._initialized = True
-            for name, param, to_freeze in self.selector.iter():
+            for name, module, param, to_freeze in self.selector.iter():
                 if to_freeze:
                     logger.info("Freezing layer %s", name)
                     param.requires_grad = False
+
+
+class LayerSharer(InitializationTrainingHook):
+    """This training hook class can be used to freeze a subset of model
+    parameters"""
+
+    source: Param[ParametersIterator]
+    """The parameters to share"""
+
+    target: Param[ParametersIterator]
+    """The parameters to be shared"""
+
+    def __init__(self):
+        self._initialized = False
+
+    def after(self, state: TrainState):
+        if not self._initialized:
+            self._initialized = True
+            for source, target in zip(
+                self.source.selected(), self.target.selected(), strict=True
+            ):
+                logger.info("Sharing layer %s -> %s", source.name, target.name)
+                target.set(source.parameter)
