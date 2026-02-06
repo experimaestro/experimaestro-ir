41d448c455e0b4eab1a78cd88cd96c51c8d81df3
mat_vast <vast@gpu.scai.sorbonne-universite.fr>
2026-02-06 17:31:05 +0100

    Add the top50 version (supposed to be the best one)

--- src/xpmir/dm/config/com/github/webis-de/rank-distillm.py
+++ src/xpmir/dm/config/com/github/webis-de/rank-distillm.py
@@ -3,7 +3,7 @@ from hashlib import md5
 from datamaestro.definitions import dataset
 from datamaestro.download.single import filedownloader
 from datamaestro.utils import HashCheck
-from xpmir.letor.distillation.samplers import ListwiseDistillationSamplesTSV
+from xpmir.letor.distillation.samplers import ListwiseDistillationSamplesTSV, ListwiseDistillationSamplesTSVWithAnnotations
 
 
 @filedownloader(
@@ -13,17 +13,19 @@ from xpmir.letor.distillation.samplers import ListwiseDistillationSamplesTSV
     checker=HashCheck("835372b2ab4d20acf10addeae526c559", md5),
 )
 @dataset(
-    ListwiseDistillationSamplesTSV,
+    ListwiseDistillationSamplesTSVWithAnnotations,
     url="https://github.com/webis-de/rank-distillm",
 )
-def msmarco_bm25_annotated(bm25__msmarco_passage_train_judged_run):
+def msmarco_bm25_annotated(bm25__msmarco_passage_train_judged):
     """Top 500 passages for all queries that have at least one relevance judgement 
     in the MS MARCO training query set retrieved by BM25.
     """
     return {
+        "qrels_file": "irds.msmarco-passage.train.qrels",
+        "top_k": 500,
         "with_docid": True,
         "with_queryid": True,
-        "path": bm25__msmarco_passage_train_judged_run,
+        "path": bm25__msmarco_passage_train_judged,
     }
 
 @filedownloader(
@@ -33,17 +35,19 @@ def msmarco_bm25_annotated(bm25__msmarco_passage_train_judged_run):
     checker=HashCheck("6ed152027f7270f32fcbfaaa6def951e", md5),
 )
 @dataset(
-    ListwiseDistillationSamplesTSV,
+    ListwiseDistillationSamplesTSVWithAnnotations,
     url="https://github.com/webis-de/rank-distillm",
 )
-def msmarco_colbertv2_annotated(colbert__msmarco_passage_train_judged_run):
+def msmarco_colbertv2_annotated(colbert__msmarco_passage_train_judged):
     """Top 500 passages for all queries that have at least one relevance judgement 
     in the MS MARCO training query set retrieved by ColBERTv2.
     """
     return {
+        "qrels_file": "irds.msmarco-passage.train.qrels",
+        "top_k": 500,
         "with_docid": True,
         "with_queryid": True,
-        "path": colbert__msmarco_passage_train_judged_run,
+        "path": colbert__msmarco_passage_train_judged,
     }
 
 
@@ -57,7 +61,7 @@ def msmarco_colbertv2_annotated(colbert__msmarco_passage_train_judged_run):
     ListwiseDistillationSamplesTSV,
     url="https://github.com/webis-de/rank-distillm",
 )
-def rankzephyr_bm25_10000_sampled_100_annotated(rankzephyr_bm25_10000_sampled_100__msmarco_passage_train_judged_run):
+def rankzephyr_bm25_10000_sampled_100_annotated(rankzephyr_bm25_10000_sampled_100__msmarco_passage_train_judged):
     """Top 100 passages retrieved by BM25 for 10k queries sampled from the MSMARCO training set.
 
     All passages are then reranked using RankZephyr and can be used for distillation.
@@ -66,12 +70,12 @@ def rankzephyr_bm25_10000_sampled_100_annotated(rankzephyr_bm25_10000_sampled_10
         "top_k": 100,
         "with_docid": True,
         "with_queryid": True,
-        "path": rankzephyr_bm25_10000_sampled_100__msmarco_passage_train_judged_run,
+        "path": rankzephyr_bm25_10000_sampled_100__msmarco_passage_train_judged,
     }
 
 
 @filedownloader(
-    "rankzephyr_colbert_10000_sampled_100__msmarco_passage_train_judged_run",
+    "rankzephyr_colbert_10000_sampled_100__msmarco_passage_train_judged.run",
     "https://zenodo.org/records/12528410/files/"
     "__rankzephyr-colbert-10000-sampled-100__msmarco-passage-train-judged.run?download=1",
     checker=HashCheck("49f8dbf2c1ee7a2ca1fe517eda528af6  ", md5),
@@ -80,8 +84,8 @@ def rankzephyr_bm25_10000_sampled_100_annotated(rankzephyr_bm25_10000_sampled_10
     ListwiseDistillationSamplesTSV,
     url="https://github.com/webis-de/rank-distillm",
 )
-def rankzephyr_colbert_10000_sampled_100_annotated(rankzephyr_colbert_10000_sampled_100__msmarco_passage_train_judged_run):
-    """Top 100 passages retrieved by BM25 for 10k queries sampled from the MSMARCO training set.
+def rankzephyr_colbert_10000_sampled_100_annotated(rankzephyr_colbert_10000_sampled_100__msmarco_passage_train_judged):
+    """Top 100 passages retrieved by ColBERT for 10k queries sampled from the MSMARCO training set.
 
     All passages are then reranked using RankZephyr and can be used for distillation.
     """
@@ -89,9 +93,31 @@ def rankzephyr_colbert_10000_sampled_100_annotated(rankzephyr_colbert_10000_samp
         "top_k": 100,
         "with_docid": True,
         "with_queryid": True,
-        "path": rankzephyr_colbert_10000_sampled_100__msmarco_passage_train_judged_run,
+        "path": rankzephyr_colbert_10000_sampled_100__msmarco_passage_train_judged,
     }
 
+
+@filedownloader(
+    "rankzephyr_colbert_10000_sampled_50__msmarco_passage_train_judged.run",
+    "https://zenodo.org/records/12528410/files/"
+    "__rankzephyr-colbert-10000-sampled-50__msmarco-passage-train-judged.run?download=1",
+    checker=HashCheck("c37b78874d4893a00566ab40aa453c56", md5),
+)
+@dataset(
+    ListwiseDistillationSamplesTSV,
+    url="https://github.com/webis-de/rank-distillm",
+)
+def rankzephyr_colbert_10000_sampled_50_annotated(rankzephyr_colbert_10000_sampled_50__msmarco_passage_train_judged):
+    """Top 50 passages retrieved by ColBERT for 10k queries sampled from the MSMARCO training set.
+
+    All passages are then reranked using RankZephyr and can be used for distillation.
+    """
+    return {
+        "top_k": 50,
+        "with_docid": True,
+        "with_queryid": True,
+        "path": rankzephyr_colbert_10000_sampled_50__msmarco_passage_train_judged,
+    }
 
 @filedownloader(
     "rankzephyr_colbert_10000_sampled_10__msmarco_passage_train_judged.run",
@@ -103,7 +129,7 @@ def rankzephyr_colbert_10000_sampled_100_annotated(rankzephyr_colbert_10000_samp
     ListwiseDistillationSamplesTSV,
     url="https://github.com/webis-de/rank-distillm",
 )
-def rankzephyr_colbert_10000_sampled_10_annotated(rankzephyr_colbert_10000_sampled_10__msmarco_passage_train_judged_run):
+def rankzephyr_colbert_10000_sampled_10_annotated(rankzephyr_colbert_10000_sampled_10__msmarco_passage_train_judged):
     """Top 10 passages retrieved by ColBERT for 10k queries sampled from the MSMARCO training set.
 
     All passages are then reranked using RankZephyr and can be used for distillation.
@@ -112,5 +138,5 @@ def rankzephyr_colbert_10000_sampled_10_annotated(rankzephyr_colbert_10000_sampl
         "top_k": 10,
         "with_docid": True,
         "with_queryid": True,
-        "path": rankzephyr_colbert_10000_sampled_10__msmarco_passage_train_judged_run,
+        "path": rankzephyr_colbert_10000_sampled_10__msmarco_passage_train_judged,
     }
